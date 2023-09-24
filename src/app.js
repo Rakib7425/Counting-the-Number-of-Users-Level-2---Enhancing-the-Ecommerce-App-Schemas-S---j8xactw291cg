e00a1b26cb51e9773e1bba2b5bb3c24468129fdb
Rakibul Islam <103498045+Rakib7425@users.noreply.github.com>
2023-09-24 12:33:37 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -19,53 +19,32 @@ app.use(express.json());
 
 // Complete this Route which will return the count of Number of Prefixmatch for the name in the query/
 
-app.get("/", async (req, res) => {
+app.get("/", async function (req, res) {
 	// https://my.newtonschool.co/playground/project/j8xactw291cg
-	try {
-		const { name } = req.query;
-		var count = 0;
-		if (!name) {
-			count = await User.find({}).count();
-		} else {
-			const regex = new RegExp(`^${name}`, "i");
-			count = await User.find({ username: { $regex: regex } }).count();
-			// count = await User.find({ name: { $regex: regex } }).count();
+	var name = req.query.name,
+		count = 0;
+	result = await users.find({});
+	if (typeof name === "undefined") {
+		count = result.length;
+		res.send(JSON.stringify(count));
+	} else {
+		name = name.toLowerCase();
+		for (var i = 0; i < result.length; i++) {
+			var len = name.length,
+				match = 1,
+				cur_name = result[i]["name"].toLowerCase();
+			if (cur_name.length >= len) {
+				for (var j = 0; j < len; j++) {
+					if (cur_name[j] != name[j]) {
+						match = 0;
+						break;
+					}
+				}
+			}
+			count += match;
 		}
-		Number(count);
-		// console.log(typeof count);
-		res.send({ count });
-		
-	} catch (error) {
-		console.error("Error:", error);
-		res.status(500).json({ error: "Internal server error" });
+		res.send(JSON.stringify(count));
 	}
 });
-
-// app.get('/', async (req, res) => {
-//   try {
-//     const { name } = req.query; // Get the "name" parameter from the query
-//     console.log('Name:', name);
-
-//     let userss;
-
-//     if (!name) {
-//       // If no "name" parameter is provided, return all users
-//       userss = await users.find();
-//     } else {
-//       const regex = new RegExp(`^${name}`, 'i');
-//       userss = await users.find({ name: { $regex: regex } });
-//     }
-
-//     // console.log('Users:', users);
-
-//     const count = userss.length; 
-//     res.json({ count });
-
-	  
-//   } catch (error) {
-//     console.error('Error:', error);
-//     res.status(500).json({ error: 'Internal server error' });
-//   }
-// });
 
 module.exports = app;
